2df515d1f3531234554609ccdced21273a3e47c3
Justin Littman <justinlittman@gwu.edu>
2015-05-21 13:14:05 -0400

    Further buildout of bio and added unit test for bio.

--- app/bio.py
+++ app/bio.py
@@ -1,29 +1,58 @@
 from vivo_namespace import VIVO
-from rdflib import RDFS, RDF, Literal
+from rdflib import RDFS, RDF, Literal, XSD
 from utility import join_if_not_empty
 from vivo_namespace import VCARD, OBO, FOAF
 
 
-def crosswalk_bio(orcid_profile, person_uri, graph, skip_person=False, person_class=FOAF.Person):
+def crosswalk_bio(orcid_profile, person_uri, graph, skip_person=False, person_class=FOAF.Person,
+                  existing_vcard_uri=None, skip_name_vcard=False):
+
+    #Get names (for person and name vcard)
+    person_details = orcid_profile["orcid-profile"]["orcid-bio"].get("personal-details", {})
+    given_names = person_details.get("given-names", {}).get("value")
+    family_name = person_details.get("family-name", {}).get("value")
+    full_name = join_if_not_empty((given_names, family_name))
+
+    #Following is non-vcard bio information
 
     #If skip_person, then don't create person and add names
     if not skip_person:
-        person_details = orcid_profile["orcid-profile"]["orcid-bio"]["personal-details"]
-        given_names = person_details["given-names"]["value"] if "given-names" in person_details else None
-        family_name = person_details["family-name"]["value"] if "family-name" in person_details else None
-        full_name = join_if_not_empty((given_names, family_name))
-
-        ##Person
+        #Add person
         graph.add((person_uri, RDF.type, person_class))
         graph.add((person_uri, RDFS.label, Literal(full_name)))
-        #Note that not assigning class here.
 
-        ##vcard
-        #Main vcard
-        vcard_uri = person_uri + "-vcard"
-        graph.add((vcard_uri, RDF.type, VCARD.Individual))
-        #Contact info for
-        graph.add((vcard_uri, OBO.ARG_2000029, person_uri))
+    #Biography
+    biography = (orcid_profile["orcid-profile"]["orcid-bio"].get("biography") or {}).get("value")
+    if biography:
+        graph.add((person_uri, VIVO.overview, Literal(biography)))
+
+    #Other identifiers
+    #Default VIVO-ISF only supports a limited number of identifier types.
+    external_identifiers = \
+        (orcid_profile["orcid-profile"]["orcid-bio"].get("external-identifiers", {}) or {}).get("external-identifier", [])
+    for external_identifier in external_identifiers:
+        #Scopus ID
+        if external_identifier["external-id-common-name"]["value"] == "Scopus Author ID":
+            graph.add((person_uri, VIVO.scopusId, Literal(external_identifier["external-id-reference"]["value"])))
+
+        #ISI Research ID
+        if external_identifier["external-id-common-name"]["value"] == "ResearcherID":
+            graph.add((person_uri, VIVO.researcherId, Literal(external_identifier["external-id-reference"]["value"])))
+
+    #Keywords
+    keywords =  \
+        (orcid_profile["orcid-profile"]["orcid-bio"].get("keywords", {}) or {}).get("keyword", [])
+    for keyword in keywords:
+        graph.add((person_uri, VIVO.freetextKeyword, Literal(keyword["value"])))
+
+    #Following is vcard bio information
+
+    #Add main vcard
+    vcard_uri = existing_vcard_uri or person_uri + "-vcard"
+    #Will only add vcard if there is a child vcard
+    add_main_vcard = False
+
+    if not skip_name_vcard and (given_names or family_name):
         #Name vcard
         vcard_name_uri = person_uri + "-vcard-name"
         graph.add((vcard_name_uri, RDF.type, VCARD.Name))
@@ -32,12 +61,22 @@ def crosswalk_bio(orcid_profile, person_uri, graph, skip_person=False, person_cl
             graph.add((vcard_name_uri, VCARD.givenName, Literal(given_names)))
         if family_name:
             graph.add((vcard_name_uri, VCARD.familyName, Literal(family_name)))
+        add_main_vcard = True
 
-    #Other identifiers
-    if "external-identifiers" in orcid_profile["orcid-profile"]["orcid-bio"] \
-            and orcid_profile["orcid-profile"]["orcid-bio"]["external-identifiers"] \
-            and "external-identifier" in orcid_profile["orcid-profile"]["orcid-bio"]["external-identifiers"]:
-        external_identifiers = orcid_profile["orcid-profile"]["orcid-bio"]["external-identifiers"]["external-identifier"]
-        for external_identifier in external_identifiers:
-            if external_identifier["external-id-common-name"]["value"] == "Scopus Author ID":
-                graph.add((person_uri, VIVO.scopusId, Literal(external_identifier["external-id-reference"]["value"])))
+    #Websites
+    researcher_urls = \
+        (orcid_profile["orcid-profile"]["orcid-bio"].get("researcher-urls", {}) or {}).get("researcher-url", [])
+    for index, researcher_url in enumerate(researcher_urls):
+        url = researcher_url["url"]["value"]
+        url_name = (researcher_url["url-name"] or {}).get("value")
+        vcard_website_uri = person_uri + "-vcard-website" + str(index)
+        graph.add((vcard_website_uri, RDF.type, VCARD.URL))
+        graph.add((vcard_uri, VCARD.hasURL, vcard_website_uri))
+        graph.add((vcard_website_uri, VCARD.url, Literal(url, datatype=XSD.anyURI)))
+        if url_name:
+            graph.add((vcard_website_uri, RDFS.label, Literal(url_name)))
+
+    if add_main_vcard and not existing_vcard_uri:
+        graph.add((vcard_uri, RDF.type, VCARD.Individual))
+        #Contact info for
+        graph.add((vcard_uri, OBO.ARG_2000029, person_uri))
---
+++ tests/app/test_bio.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+import json
+import app.bio as bio
+import app.vivo_namespace as ns
+from rdflib import Literal, Graph, RDF, RDFS
+from app.vivo_namespace import D, VIVO, FOAF
+
+
+class TestBio(TestCase):
+
+    def setUp(self):
+        self.graph = Graph(namespace_manager=ns.ns_manager)
+        self.person_uri = ns.D["test"]
+
+    def test_no_external_identifiers(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "external-identifiers": null
+        }
+    }
+}
+        """)
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+        self.assertEqual(0, len(self.graph))
+
+    def test_external_identifiers(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "external-identifiers": {
+                "external-identifier": [
+                    {
+                        "orcid": null,
+                        "external-id-orcid": null,
+                        "external-id-common-name": {
+                            "value": "ISNI"
+                        },
+                        "external-id-reference": {
+                            "value": "0000000138352317"
+                        },
+                        "external-id-url": {
+                            "value": "http://isni.org/isni/0000000138352317"
+                        },
+                        "external-id-source": null,
+                        "source": {
+                            "source-orcid": {
+                                "value": null,
+                                "uri": "http://orcid.org/0000-0003-0412-1857",
+                                "path": "0000-0003-0412-1857",
+                                "host": "orcid.org"
+                            },
+                            "source-client-id": null,
+                            "source-name": null,
+                            "source-date": null
+                        }
+                    },
+                    {
+                        "orcid": null,
+                        "external-id-orcid": null,
+                        "external-id-common-name": {
+                            "value": "ResearcherID"
+                        },
+                        "external-id-reference": {
+                            "value": "C-4986-2008"
+                        },
+                        "external-id-url": {
+                            "value": "http://www.researcherid.com/rid/C-4986-2008"
+                        },
+                        "external-id-source": null,
+                        "source": {
+                            "source-orcid": {
+                                "value": null,
+                                "uri": "http://orcid.org/0000-0001-7707-4137",
+                                "path": "0000-0001-7707-4137",
+                                "host": "orcid.org"
+                            },
+                            "source-client-id": null,
+                            "source-name": null,
+                            "source-date": null
+                        }
+                    },
+                    {
+                        "orcid": null,
+                        "external-id-orcid": null,
+                        "external-id-common-name": {
+                            "value": "Scopus Author ID"
+                        },
+                        "external-id-reference": {
+                            "value": "6602258586"
+                        },
+                        "external-id-url": {
+                            "value": "http://www.scopus.com/inward/authorDetails.url?authorID=6602258586&partnerID=MN8TOARS"
+                        },
+                        "external-id-source": null,
+                        "source": {
+                            "source-orcid": {
+                                "value": null,
+                                "uri": "http://orcid.org/0000-0002-5982-8983",
+                                "path": "0000-0002-5982-8983",
+                                "host": "orcid.org"
+                            },
+                            "source-client-id": null,
+                            "source-name": null,
+                            "source-date": null
+                        }
+                    }
+                ],
+                "visibility": "PUBLIC"
+            }
+        }
+    }
+}
+        """)
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+        self.assertEqual(2, len(self.graph))
+        #ScopusID is added.
+        self.assertTrue(self.graph[D["test"]: VIVO["scopusId"] : Literal("6602258586")])
+        #ResearcherId is added.
+        self.assertTrue(self.graph[D["test"]: VIVO["researcherId"] : Literal("C-4986-2008")])
+
+    def test_name(self):
+        orcid_profile = json.loads(u"""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "personal-details": {
+                "given-names": {
+                    "value": "Laurel"
+                },
+                "family-name": {
+                    "value": "Haak"
+                },
+                "credit-name": {
+                    "value": "Laurel L Haak",
+                    "visibility": "PUBLIC"
+                },
+                "other-names": {
+                    "other-name": [
+                        {
+                            "value": " L. L. Haak"
+                        },
+                        {
+                            "value": "L Haak"
+                        },
+                        {
+                            "value": "Laure Haak"
+                        },
+                        {
+                            "value": "Laurela L Hāka"
+                        }
+                    ],
+                    "visibility": "PUBLIC"
+                }
+            },
+            "external-identifiers": null
+        }
+    }
+}
+        """)
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph)
+        #Laurel is a person
+        self.assertTrue(self.graph[D["test"]: RDF.type: FOAF.Person])
+        #with a label
+        self.assertTrue(self.graph[D["test"]: RDFS.label: Literal("Laurel Haak")])
+
+        #vcard test
+        self.assertTrue(bool(self.graph.query("""
+            ask where {
+                ?vcn a vcard:Name .
+                ?vcn vcard:familyName "Haak" .
+                ?vcn vcard:givenName "Laurel" .
+                ?vc obo:ARG_2000029 d:test .
+                ?vc vcard:hasName ?vcn .
+            }
+        """)))
+
+    def test_biography(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "biography": {
+                "value": "Laurel L. Haak, PhD, is the Executive Director of ORCID.",
+                "visibility": "PUBLIC"
+            }
+        }
+    }
+}
+        """)
+
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+
+        #Has a biography
+        self.assertTrue(self.graph[D["test"]: VIVO["overview"]:
+            Literal("Laurel L. Haak, PhD, is the Executive Director of ORCID.")])
+
+    def test_no_biography(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "biography": null
+        }
+    }
+}
+        """)
+
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+
+        #Has a biography
+        self.assertEqual(0, len(self.graph))
+
+    def test_websites(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "researcher-urls": {
+                "researcher-url": [
+                    {
+                        "url-name": {
+                            "value": "LinkedIn"
+                        },
+                        "url": {
+                            "value": "http://www.linkedin.com/pub/laurel-haak/3/1b/4a3/"
+                        }
+                    },
+                    {
+                        "url-name": null,
+                        "url": {
+                            "value": "https://www.researchgate.net/profile/Laurel_Haak"
+                        }
+                    }
+                ],
+                "visibility": "PUBLIC"
+            },
+            "contact-details": {
+                "email": [],
+                "address": {
+                    "country": {
+                        "value": "US",
+                        "visibility": "PUBLIC"
+                    }
+                }
+            }
+        }
+    }
+}
+        """)
+
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+
+        #LinkedIn
+        self.assertTrue(bool(self.graph.query("""
+            ask where {
+                ?vcw a vcard:URL .
+                ?vcw vcard:url "http://www.linkedin.com/pub/laurel-haak/3/1b/4a3/"^^xsd:anyURI .
+                ?vcw rdfs:label "LinkedIn" .
+                d:test-vcard vcard:hasURL ?vcw .
+            }
+        """)))
+
+        #ResearchGate
+        self.assertTrue(bool(self.graph.query("""
+            ask where {
+                ?vcw a vcard:URL .
+                ?vcw vcard:url "https://www.researchgate.net/profile/Laurel_Haak"^^xsd:anyURI .
+                d:test-vcard vcard:hasURL ?vcw .
+                filter not exists {
+                    ?vcw rdfs:label ?label .
+                }
+            }
+        """)))
+
+    def test_no_websites(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "researcher-urls": {
+                "researcher-url": [],
+                "visibility": "PUBLIC"
+            }
+        }
+    }
+}
+        """)
+
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+
+        #Has a biography
+        self.assertEqual(0, len(self.graph))
+
+    def test_no_keywords(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "keywords": null
+        }
+    }
+}
+        """)
+
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+
+        #Has a biography
+        self.assertEqual(0, len(self.graph))
+
+    def test_keywords(self):
+        orcid_profile = json.loads("""
+{
+    "message-version": "1.2",
+    "orcid-profile": {
+        "orcid-bio": {
+            "keywords": {
+                "keyword": [
+                    {
+                        "value": "persistent identifiers"
+                    },
+                    {
+                        "value": "research policy"
+                    },
+                    {
+                        "value": "science workforce"
+                    }
+                ],
+                "visibility": "PUBLIC"
+            }
+        }
+    }
+}
+        """)
+
+        bio.crosswalk_bio(orcid_profile, self.person_uri, self.graph, skip_person=True)
+        self.assertEqual(3, len(self.graph))
+
+        self.assertTrue(bool(self.graph.query("""
+            ask where {
+                d:test vivo:freetextKeyword "persistent identifiers" .
+                d:test vivo:freetextKeyword "research policy" .
+                d:test vivo:freetextKeyword "science workforce" .
+            }
+        """)))
