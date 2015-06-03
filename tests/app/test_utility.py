24f2cbcde1ff146bf16ad535181f86c7fbb48f16
Ted Lawless <lawlesst@gmail.com>
2015-06-03 16:57:54 -0400

    Addresses #16.  Allows orcid.org namespace to be added in web form.  Adds utility for cleaning submitted ORCIDS.  Adds test.

---
+++ tests/app/test_utility.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from app.utility import clean_orcid
+
+
+class TestUtility(TestCase):
+
+    def test_clean_orcid(self):
+        orcid = '0000-0003-1527-0030'
+
+        # Test with orcid.org prefix.
+        self.assertEqual(clean_orcid('orcid.org/' + orcid), orcid)
+
+        # Test without prefix.
+        self.assertEqual(clean_orcid(orcid), orcid)
